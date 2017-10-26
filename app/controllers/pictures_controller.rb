3b9d961b10778138dd83ff32031aac0c784a60f7
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-26 02:19:43 -0400

    setup the index and show page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,20 +1,37 @@
 class PicturesController < ApplicationController
+  before_action :find_picture, only:[:show, :edit, :destroy]
+  before_action :new_picture, only:[:create, :new]
+
   def index
+    @pictures = Picture.all
   end
 
   def show
+    # find_picture
   end
 
   def edit
+    # find_picture
   end
 
   def mew
+    # new_picture
   end
 
   def create
+    # new_picture
   end
 
   def destroy
+    # find_picture
   end
-  
+
+  def new_picture
+    @picture = Picture.new
+  end
+
+  def find_picture
+    @picture = Picture.find(params[:id])
+  end
+
 end
