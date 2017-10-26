35dbe106eca4a9aabcde85e5c3889ce4f7c728b8
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-26 01:26:02 -0400

    generated model and the controller

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,20 @@
+class PicturesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def mew
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
+end
